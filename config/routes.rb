9108039dc27547aa56cc7d2c06e496e98bf2393e
John Hwang <johnsies87@gmail.com>
2016-01-07 16:02:51 -0800

    css stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
 
   resources :friendships, only: [:create, :destroy]
   get 'friendships/create'
-
   get 'friendships/destroy'
-
   root 'users#index', as: :users
-
   post 'users/new' => 'users#new', as: :new_user
   get 'search/sort' => 'users#sort', as: :users_sort
   post '/' => 'users#create'
@@ -21,27 +18,17 @@ Rails.application.routes.draw do
   get 'jams/:id/edit' => 'jams#edit', as: :edit_jam
   patch 'jams/:id' => 'jams#update'
   delete 'jams/:id' => 'jams#destroy'
-
   get 'search1/sort' => 'jams#sort', as: :jams_sort
-
   get 'jams/all' => 'jams#index', as: :jams
   post 'jams/new' => 'jams#create', as: :create_jam
-
-
-
-
   get 'search1' => 'jams#search', as: 'search1'
   get 'search' => 'users#search', as: 'search'
   # get 'search_results' => 'users#search_results', as: 'search_results'
   # get 'sessions/users/:id' => 'sessions#show', as: :show_session
   # get 'targets/' => 'targets#index', as: :targets
-
   get 'sessions/new' => 'sessions#new', as: :new_session
-
   post 'sessions/new' => 'sessions#create', as: :create_session
-
   get 'sessions/destroy' => 'sessions#destroy', as: :destroy_session
-
 
   namespace :api do
     resources :jams, only: [:index, :show]
